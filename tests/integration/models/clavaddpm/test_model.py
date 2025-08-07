3b8619e5d9bd036df7956dfa7cea02007ed9c89e
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2025-08-07 18:19:58 -0300

    Adding more detailed integration tests for clustering

--- tests/integration/models/clavaddpm/test_model.py
+++ tests/integration/models/clavaddpm/test_model.py
@@ -6,6 +6,7 @@ import pytest
 
 from midst_toolkit.core.data_loaders import load_multi_table
 from midst_toolkit.models.clavaddpm.model import clava_clustering, clava_training
+from tests.utils.random import set_all_random_seeds, unset_all_random_seeds
 
 
 CLUSTERING_CONFIG = {
@@ -53,30 +54,12 @@ def test_load_single_table():
         "account",
     ]
     assert tables["trans"]["df"].shape == (99, 8)
-    with open("tests/integration/data/multi_table/trans_domain.json", "r") as f:
+    assert tables["trans"]["df"].equals(tables["trans"]["original_df"])
+    assert tables["trans"]["df"].columns.tolist() == tables["trans"]["original_cols"]
+    with open("tests/integration/data/single_table/trans_domain.json", "r") as f:
         assert tables["trans"]["domain"] == json.load(f)
     assert tables["trans"]["children"] == []
     assert tables["trans"]["parents"] == []
-    assert tables["trans"]["original_cols"] == [
-        "trans_date",
-        "trans_type",
-        "operation",
-        "amount",
-        "balance",
-        "k_symbol",
-        "bank",
-        "account",
-    ]
-    assert tables["trans"]["original_df"].columns.tolist() == [
-        "trans_date",
-        "trans_type",
-        "operation",
-        "amount",
-        "balance",
-        "k_symbol",
-        "bank",
-        "account",
-    ]
     assert tables["trans"]["info"] == {
         "num_col_idx": [0, 3, 4, 7],
         "cat_col_idx": [1, 2, 5, 6],
@@ -137,17 +120,12 @@ def test_load_multi_table():
 
     assert tables["account"]["df"].columns.tolist() == ["account_id", "district_id", "frequency", "account_date"]
     assert tables["account"]["df"].shape == (9, 4)
+    assert tables["account"]["df"].equals(tables["account"]["original_df"])
+    assert tables["account"]["df"].columns.tolist() == tables["account"]["original_cols"]
     with open("tests/integration/data/multi_table/account_domain.json", "r") as f:
         assert tables["account"]["domain"] == json.load(f)
     assert tables["account"]["children"] == ["trans"]
     assert tables["account"]["parents"] == []
-    assert tables["account"]["original_cols"] == ["account_id", "district_id", "frequency", "account_date"]
-    assert tables["account"]["original_df"].columns.tolist() == [
-        "account_id",
-        "district_id",
-        "frequency",
-        "account_date",
-    ]
     assert tables["account"]["info"] == {
         "num_col_idx": [1],
         "cat_col_idx": [0],
@@ -187,23 +165,13 @@ def test_load_multi_table():
         "account",
     ]
     assert tables["trans"]["df"].shape == (143, 10)
+    assert tables["trans"]["df"].equals(tables["trans"]["original_df"])
+    assert tables["trans"]["df"].columns.tolist() == tables["trans"]["original_cols"]
     with open("tests/integration/data/multi_table/trans_domain.json", "r") as f:
         assert tables["trans"]["domain"] == json.load(f)
     assert tables["trans"]["children"] == []
     assert tables["trans"]["parents"] == ["account"]
     assert tables["trans"]["original_cols"] == [
-        "trans_id",
-        "account_id",
-        "trans_date",
-        "trans_type",
-        "operation",
-        "amount",
-        "balance",
-        "k_symbol",
-        "bank",
-        "account",
-    ]
-    assert tables["trans"]["original_df"].columns.tolist() == [
         "trans_id",
         "account_id",
         "trans_date",
@@ -295,15 +263,48 @@ def test_train_multi_table(tmp_path: Path):
 
 @pytest.mark.integration_test()
 def test_clustering_reload(tmp_path: Path):
+    set_all_random_seeds()
+
     os.makedirs(tmp_path / "models")
     configs = {"clustering": CLUSTERING_CONFIG}
 
     tables, relation_order, dataset_meta = load_multi_table("tests/integration/data/multi_table/")
     tables, all_group_lengths_prob_dicts = clava_clustering(tables, relation_order, tmp_path, configs)
 
-    assert all_group_lengths_prob_dicts
+    account_df_no_clustering = tables["account"]["df"].drop(columns=["account_trans_cluster"])
+    account_original_df_as_float = tables["account"]["original_df"].astype(float)
+    assert account_df_no_clustering.equals(account_original_df_as_float)
+    with open("tests/integration/data/multi_table/assertion_data/expected_account_clustering.json", "r") as f:
+        expected_account_clustering = json.load(f)
+    assert tables["account"]["df"]["account_trans_cluster"].tolist() == expected_account_clustering
+
+    trans_df_no_clustering = tables["trans"]["df"].drop(columns=["account_trans_cluster"])
+    trans_original_df_as_float = tables["trans"]["original_df"].astype(float)
+    trans_original_df_as_float["trans_id"] = trans_original_df_as_float["trans_id"].astype(int)
+    assert trans_df_no_clustering.equals(trans_original_df_as_float)
+    with open("tests/integration/data/multi_table/assertion_data/expected_trans_clustering.json", "r") as f:
+        expected_trans_clustering = json.load(f)
+    assert tables["trans"]["df"]["account_trans_cluster"].tolist() == expected_trans_clustering
 
     # loading from previously saved clustering
-    tables, all_group_lengths_prob_dicts = clava_clustering(tables, relation_order, tmp_path, configs)
+    tables_saved, all_group_lengths_prob_dicts_saved = clava_clustering(tables, relation_order, tmp_path, configs)
 
-    assert all_group_lengths_prob_dicts
+    assert all_group_lengths_prob_dicts_saved == all_group_lengths_prob_dicts
+
+    assert tables_saved["account"]["df"].equals(tables["account"]["df"])
+    assert tables_saved["account"]["original_df"].equals(tables["account"]["original_df"])
+    assert tables_saved["account"]["original_cols"] == tables["account"]["original_cols"]
+    assert tables_saved["account"]["domain"] == tables["account"]["domain"]
+    assert tables_saved["account"]["children"] == tables["account"]["children"]
+    assert tables_saved["account"]["parents"] == tables["account"]["parents"]
+    assert tables_saved["account"]["info"] == tables["account"]["info"]
+
+    assert tables_saved["trans"]["df"].equals(tables["trans"]["df"])
+    assert tables_saved["trans"]["original_df"].equals(tables["trans"]["original_df"])
+    assert tables_saved["trans"]["original_cols"] == tables["trans"]["original_cols"]
+    assert tables_saved["trans"]["domain"] == tables["trans"]["domain"]
+    assert tables_saved["trans"]["children"] == tables["trans"]["children"]
+    assert tables_saved["trans"]["parents"] == tables["trans"]["parents"]
+    assert tables_saved["trans"]["info"] == tables["trans"]["info"]
+
+    unset_all_random_seeds()
