06ff85a9fccd31868cdd181bf4e10d17a36c1c8c
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-09-08 13:12:32 -0400

    Median DCR Implementation (#17)
    * Intermediate checkin
    
    * Finalizing alpha precision code and tests
    
    * Adding period
    
    * Small changes
    
    * Dropping accidental upload
    
    * Pulling in the Synthcity encoder and ensuring that it reproduces the expected metrics.
    
    * Small bug fix
    
    * Small bug fix
    
    * Fixing fluctuation by loosening tolerance.
    
    * Small changes to log all values
    
    * Adding architecture specific testing
    
    * Upgrading torch to avoid vulnerabilities
    
    * Temporarily ignoring torch vulnerabilites. Will fix in a follow up PR
    
    * Dropping pydantic
    
    * Fixing a bug
    
    * Some small improvements
    
    * Some small improvements.
    
    * Fixing wonky issue with the feature encoder design
    
    * Fixing wonky issue with the wrapped encoder design
    
    * Tightening up the DateTimeEncoder as well.
    
    * A couple of cleanup changes.
    
    * DCR Score implementation
    
    * Small fixes
    
    * Implementing the median dcr score
    
    * Addressing various PR Comments
    
    * More PR changes suggested by Marcelo
    
    * Adding the documentation suggested by Marcelo for the Synthcity port
    
    * Small fixes associated with PR comments
    
    * docstring update that I missed
    
    * Fixing test bug
    
    * Weird bug fix...
    
    * Fixing tests pointed out in review

---
+++ tests/unit/evaluation/generation/test_median_dcr.py
@@ -0,0 +1,65 @@
+import math
+
+import pandas as pd
+import pytest
+import torch
+
+from midst_toolkit.data_processing.midst_data_processing import load_midst_data
+from midst_toolkit.evaluation.generation_quality.distance_closest_record import (
+    NormType,
+    median_distance_to_closest_record_score,
+    minimum_distances,
+    preprocess_for_distance_to_closest_record_score,
+)
+
+
+SYNTHETIC_DATA = torch.Tensor([[1.0, 2.0, 1.0], [1.0, 2.0, 3.0], [3.4, 1.0, 0.3]])
+REAL_DATA = torch.Tensor([[2.0, 1.0, 0.0], [1.0, 2.0, 3.0], [-1.0, 1.0, 2.0], [1.2, 2.3, 0.5]])
+
+SYNTHETIC_DATA_PATH = "tests/assets/synthetic_data_dcr.csv"
+REAL_DATA_TRAIN_PATH = "tests/assets/real_data_dcr.csv"
+META_INFO_PATH = "tests/assets/meta_info.json"
+
+
+def test_minimum_distance_no_batch_l1() -> None:
+    test_distance = minimum_distances(REAL_DATA, REAL_DATA, skip_diagonal=True)
+    target_distance = torch.Tensor([2.6, 3.0, 4.0, 2.6])
+    assert torch.allclose(test_distance, target_distance)
+
+
+def test_minimum_distance_no_batch_l2() -> None:
+    test_distance = minimum_distances(REAL_DATA, REAL_DATA, norm=NormType.L2, skip_diagonal=True)
+    target_distance = torch.Tensor(
+        [
+            math.sqrt(0.8 * 0.8 + 1.3 * 1.3 + 0.5 * 0.5),
+            math.sqrt(6),
+            math.sqrt(6),
+            math.sqrt(0.8 * 0.8 + 1.3 * 1.3 + 0.5 * 0.5),
+        ]
+    )
+    assert torch.allclose(test_distance, target_distance)
+
+
+def test_minimum_distance_l2_no_skip_diagonal() -> None:
+    test_distance = minimum_distances(REAL_DATA, REAL_DATA, norm=NormType.L1)
+    target_distance = torch.Tensor([0.0, 0.0, 0.0, 0.0])
+    assert torch.allclose(test_distance, target_distance)
+
+
+def test_median_dcr_score() -> None:
+    real_data, synthetic_data, meta_info = load_midst_data(REAL_DATA_TRAIN_PATH, SYNTHETIC_DATA_PATH, META_INFO_PATH)
+
+    real_data, _, synthetic_data = preprocess_for_distance_to_closest_record_score(
+        synthetic_data, real_data, real_data, meta_info
+    )
+    dcr_score = median_distance_to_closest_record_score(synthetic_data, real_data)
+    assert pytest.approx(dcr_score, abs=1e-8) == 6.540543187576836
+
+
+def test_median_dcr_score_dummy() -> None:
+    real_data_df = pd.DataFrame(REAL_DATA).astype(float)
+    synthetic_df = pd.DataFrame(SYNTHETIC_DATA).astype(float)
+
+    dcr_score = median_distance_to_closest_record_score(synthetic_df, real_data_df)
+
+    assert pytest.approx(dcr_score, abs=1e-6) == 1.0 / 2.6
