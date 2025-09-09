a16132aa315f15f3abbc36579ea2c074b7f0bbfe
Sara Kodeiri <sara.kodeiri@vectorinstitute.ai>
2025-09-09 15:26:32 -0400

    Add DOMIAS calculation

--- examples/ensemble_attack_example/run_attack.py
+++ examples/ensemble_attack_example/run_attack.py
@@ -66,12 +66,12 @@ def main(cfg: DictConfig) -> None:
         )
 
         df_synth = load_dataframe(
-            Path(cfg.data_paths.midst_data_path),
+            Path(cfg.data_paths.processed_attack_data_path),
             "synth.csv",
         )
 
         df_ref = load_dataframe(
-            Path(cfg.population_path),
+            Path(cfg.data_paths.population_path),
             "population_all_with_challenge_no_id.csv",
         )
 
@@ -89,24 +89,24 @@ def main(cfg: DictConfig) -> None:
             epochs=1,
         )
 
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        model_filename = f"{timestamp}_trained_metaclassifier.pkl"
-        with open(Path(cfg.model_paths.metaclassifier_model_path) / model_filename, "wb") as f:
-            pickle.dump(blending_attacker.meta_classifier_, f)
+        # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        # model_filename = f"{timestamp}_trained_metaclassifier.pkl"
+        # with open(Path(cfg.model_paths.metaclassifier_model_path) / model_filename, "wb") as f:
+        #     pickle.dump(blending_attacker.meta_classifier_, f)
 
-        # 3. Get predictions on the test set
-        final_predictions = blending_attacker.predict(
-            df_test=df_meta_test,
-            df_synth=df_synth,
-            df_ref=df_ref,
-            cat_cols=cfg.data_configs.metadata.categorical,
-            y_test=y_meta_test,
-        )
+        # # 3. Get predictions on the test set
+        # final_predictions = blending_attacker.predict(
+        #     df_test=df_meta_test,
+        #     df_synth=df_synth,
+        #     df_ref=df_ref,
+        #     cat_cols=cfg.data_configs.metadata.categorical,
+        #     y_test=y_meta_test,
+        # )
 
-        print("Final Blending++ predictions:", final_predictions)
-        # TODO: Change print to logging
-        # TODO: Save trained model
-        log(INFO, "Metaclassifier training finished.")
+        # print("Final Blending++ predictions:", final_predictions)
+        # # TODO: Change print to logging
+        # # TODO: Save trained model
+        # log(INFO, "Metaclassifier training finished.")
 
 
 if __name__ == "__main__":
