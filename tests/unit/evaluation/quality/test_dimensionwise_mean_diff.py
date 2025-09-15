85fa283f8890f03ffea7ada7c939ba3b4e1b720e
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-09-15 09:18:02 -0400

    Add Correlation, Mutual Information, and Kolmogorov-Smirnov + TVD Measures (#33)
    * Intermediate checkin
    
    * Finalizing alpha precision code and tests
    
    * Adding period
    
    * Small changes
    
    * Dropping accidental upload
    
    * Pulling in the Synthcity encoder and ensuring that it reproduces the expected metrics.
    
    * Small bug fix
    
    * Small bug fix
    
    * Fixing fluctuation by loosening tolerance.
    
    * Small changes to log all values
    
    * Adding architecture specific testing
    
    * Upgrading torch to avoid vulnerabilities
    
    * Temporarily ignoring torch vulnerabilites. Will fix in a follow up PR
    
    * Dropping pydantic
    
    * Fixing a bug
    
    * Some small improvements
    
    * Some small improvements.
    
    * Fixing wonky issue with the feature encoder design
    
    * Fixing wonky issue with the wrapped encoder design
    
    * Tightening up the DateTimeEncoder as well.
    
    * A couple of cleanup changes.
    
    * DCR Score implementation
    
    * Small fixes
    
    * Implementing the median dcr score
    
    * Addressing various PR Comments
    
    * More PR changes suggested by Marcelo
    
    * Adding the documentation suggested by Marcelo for the Synthcity port
    
    * Small fixes associated with PR comments
    
    * docstring update that I missed
    
    * Fixing test bug
    
    * Weird bug fix...
    
    * First two metrics for SynthEval quality added
    
    * Some small typos
    
    * Fixing a code error that wasn't caught with precommit checks for some reason
    
    * Renaming to ease merge
    
    * Making names consistent
    
    * Second try at refactoring structure after all the merges
    
    * Addressing some PR comments
    
    * Addressing some PR comments
    
    * PR comment changes
    
    * Addressing some PR comments
    
    * Trying a fun overload mypy trick?
    
    * Bug fix
    
    * A couple more PR comments
    
    * PR comment changes
    
    * Addressing some PR comments
    
    * Trying a fun overload mypy trick?
    
    * Bug fix
    
    * A couple more PR comments
    
    * Adding in correlation difference and associated tests
    
    * Removing the model folder until it's needed
    
    * Intermediate checkin to switch branches
    
    * Fixing some of comments pointed out by Fatemeh
    
    * More documentation for MI and adding tests
    
    * Fixing test function names
    
    * Adding in the KS/TVD measurements from SynthEval
    
    * Some touch ups for the implementations and tests
    
    * Reorganizing the evaluation module
    
    * Deleting duplicate
    
    * Syncing naming to reformat
    
    * Removing old script
    
    * Fixing a bug
    
    * Clarity to comment
    
    * typo fix

--- tests/unit/evaluation/quality/test_dimensionwise_mean_diff.py
+++ tests/unit/evaluation/quality/test_dimensionwise_mean_diff.py
@@ -4,23 +4,23 @@ import pytest
 from midst_toolkit.evaluation.quality.dimensionwise_mean_difference import DimensionwiseMeanDifference
 
 
+REAL_DATA = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 5, 6], "column_c": ["horse", "dog", "horse"]})
+SYNTHETIC_DATA = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 6, 6], "column_c": ["cat", "dog", "horse"]})
+
+
 def test_dimensionwise_mean_diff_no_preprocess() -> None:
-    real = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 5, 6], "column_c": ["horse", "dog", "horse"]})
-    synthetic = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 6, 6], "column_c": ["cat", "dog", "horse"]})
     metric = DimensionwiseMeanDifference(
         categorical_columns=["column_c"], numerical_columns=["column_a", "column_b"], do_preprocess=False
     )
 
-    score = metric.compute(real, synthetic)
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
     assert pytest.approx(0.5 * (0.0 + 1 / 3), abs=1e-8) == score["dimensionwise_mean_difference"]
 
 
 def test_dimensionwise_mean_diff_with_preprocess() -> None:
-    real = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 5, 6], "column_c": ["horse", "dog", "horse"]})
-    synthetic = pd.DataFrame({"column_a": [1, 2, 3], "column_b": [4, 6, 6], "column_c": ["cat", "dog", "horse"]})
     metric = DimensionwiseMeanDifference(
         categorical_columns=["column_c"], numerical_columns=["column_a", "column_b"], do_preprocess=True
     )
 
-    score = metric.compute(real, synthetic)
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
     assert pytest.approx(0.5 * (0.0 + 0.5 / 3.0), abs=1e-8) == score["dimensionwise_mean_difference"]
---
+++ tests/unit/evaluation/quality/test_kolmogorov_smirnov_total_variation.py
@@ -0,0 +1,125 @@
+import math
+
+import pandas as pd
+import pytest
+
+from midst_toolkit.evaluation.quality.kolmogorov_smirnov_total_variation import KolmogorovSmirnovAndTotalVariation
+
+
+REAL_DATA = pd.DataFrame(
+    {
+        "column_a": [1, 2, 3, 4, 5],
+        "column_b": [4, 5, 6, 7, 8],
+        "column_c": ["horse", "dog", "horse", "cat", "cat"],
+        "column_d": [-1, -2, -3, -2, -5],
+    }
+)
+SYNTHETIC_DATA = pd.DataFrame(
+    {
+        "column_a": [1, 2, 3, 4, 5],
+        "column_b": [4, 6, 6, -1, 1],
+        "column_c": ["cat", "dog", "horse", "cat", "river"],
+        "column_d": [10, 11, 12, 34, 1],
+    }
+)
+
+
+def test_ks_tvd_no_preprocess() -> None:
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=["column_c"],
+        numerical_columns=["column_a", "column_b", "column_d"],
+        do_preprocess=False,
+    )
+
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(1.6 / 4.0, abs=1e-8) == score["avg stat"]
+    assert pytest.approx(1.4 / 3.0, abs=1e-8) == score["avg ks"]
+    assert pytest.approx(0.2, abs=1e-8) == score["avg tvd"]
+    assert pytest.approx((1.0 + 0.873015873015873 + 1.0 + 0.007936507936507936) / 4.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 1
+    assert score["frac sigs"] == 1 / 4.0
+
+
+def test_ks_tvd_with_preprocess() -> None:
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=["column_c"],
+        numerical_columns=["column_a", "column_b", "column_d"],
+        do_preprocess=True,
+    )
+    # Should be the same, as preprocessing just shifts and scales the distributions.
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(1.6 / 4.0, abs=1e-8) == score["avg stat"]
+    assert pytest.approx(1.4 / 3.0, abs=1e-8) == score["avg ks"]
+    assert pytest.approx(0.2, abs=1e-8) == score["avg tvd"]
+    assert pytest.approx((1.0 + 0.873015873015873 + 1.0 + 0.007936507936507936) / 4.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 1
+    assert score["frac sigs"] == 1 / 4.0
+
+
+def test_one_column_left_off() -> None:
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=["column_c"],
+        numerical_columns=["column_a", "column_b"],
+        do_preprocess=True,
+    )
+
+    # Make sure the metrics don't include the column that was left off.
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(0.6 / 3.0, abs=1e-8) == score["avg stat"]
+    assert pytest.approx(0.4 / 2.0, abs=1e-8) == score["avg ks"]
+    assert pytest.approx(0.2, abs=1e-8) == score["avg tvd"]
+    assert pytest.approx((1.0 + 0.873015873015873 + 1.0) / 3.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 0
+    assert score["frac sigs"] == 0.0
+
+
+def test_ks_tvd_no_categoricals() -> None:
+    # Also leaving off column_d
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=[],
+        numerical_columns=["column_a", "column_b", "column_d"],
+        do_preprocess=True,
+    )
+
+    # Make sure the computation doesn't include the columns, we get a NaN, and nothing breaks.
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(1.4 / 3.0, abs=1e-8) == score["avg stat"]
+    assert pytest.approx(1.4 / 3.0, abs=1e-8) == score["avg ks"]
+    assert math.isnan(score["avg tvd"])
+    assert pytest.approx((1.0 + 0.873015873015873 + 0.007936507936507936) / 3.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 1
+    assert score["frac sigs"] == 1 / 3.0
+
+
+def test_ks_tvd_no_numericals() -> None:
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=["column_b", "column_c"],
+        numerical_columns=[],
+        do_preprocess=True,
+    )
+
+    # Make sure the computation doesn't include the columns, we get a NaN, and nothing breaks.
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(0.8 / 2.0, abs=1e-8) == score["avg stat"]
+    assert math.isnan(score["avg ks"])
+    assert pytest.approx(0.8 / 2.0, abs=1e-8) == score["avg tvd"]
+    assert pytest.approx((1.0 + 1.0) / 2.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 0
+    assert score["frac sigs"] == 0.0
+
+
+def test_ks_tvd_with_preprocess_new_confidence() -> None:
+    metric = KolmogorovSmirnovAndTotalVariation(
+        categorical_columns=["column_c"],
+        numerical_columns=["column_a", "column_b", "column_d"],
+        do_preprocess=True,
+        significance_level=0.9,
+    )
+    # Stats shouldn't change, but the high significance level means the 0.873015873015873 should become significant.
+    score = metric.compute(REAL_DATA, SYNTHETIC_DATA)
+    assert pytest.approx(1.6 / 4.0, abs=1e-8) == score["avg stat"]
+    assert pytest.approx(1.4 / 3.0, abs=1e-8) == score["avg ks"]
+    assert pytest.approx(0.2, abs=1e-8) == score["avg tvd"]
+    assert pytest.approx((1.0 + 0.873015873015873 + 1.0 + 0.007936507936507936) / 4.0, abs=1e-8) == score["avg pval"]
+    assert score["num sigs"] == 2
+    assert score["frac sigs"] == 2 / 4.0
