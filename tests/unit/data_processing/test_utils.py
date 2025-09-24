810f3d6f20c0a563f470c72a8c19319d89780a43
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-09-24 12:16:01 -0400

    Adding in a bit more revealing testing for the categorical column detection

--- tests/unit/data_processing/test_utils.py
+++ tests/unit/data_processing/test_utils.py
@@ -14,6 +14,13 @@ TEST_DATAFRAME = pd.DataFrame(
         "column_c": ["house", "cat", "cat", "car", "dog"],
         "column_d": [1, 1, 3, 2, 2],
         "column_e": [1.0, 3.0, 1.0, 2.0, 1.0],
+        "column_f": [
+            pd.Timestamp("2018-01-05"),
+            pd.Timestamp("2018-01-06"),
+            pd.Timestamp("2018-01-07"),
+            pd.Timestamp("2018-01-08"),
+            pd.Timestamp("2018-01-09"),
+        ],
     }
 )
 
@@ -24,15 +31,20 @@ def test_is_column_type_numerical() -> None:
     assert not is_column_type_numerical(TEST_DATAFRAME, "column_c")
     assert is_column_type_numerical(TEST_DATAFRAME, "column_d")
     assert is_column_type_numerical(TEST_DATAFRAME, "column_e")
+    assert not is_column_type_numerical(TEST_DATAFRAME, "column_f")
 
 
 def test_get_categorical_columns() -> None:
     # Low threshold
     categorical_columns = get_categorical_columns(TEST_DATAFRAME, 2)
+    # Note that this does not include the date time column, as it isn't a categorical, as the detection algorithm
+    # functions at the moment.
     assert categorical_columns == ["column_c"]
 
     # Higher threshold
     categorical_columns = get_categorical_columns(TEST_DATAFRAME, 4)
+    # Note that this does not include the date time column, as it isn't a categorical, as the detection algorithm
+    # functions at the moment.
     assert sorted(categorical_columns) == ["column_c", "column_d", "column_e"]
 
 
@@ -42,11 +54,13 @@ def test_infer_categorical_and_numerical_columns() -> None:
         TEST_DATAFRAME, categorical_threshold=2
     )
     assert categorical_columns == ["column_c"]
-    assert sorted(numerical_columns) == ["column_a", "column_b", "column_d", "column_e"]
+    # Note that this includes the date time column, as it isn't a categorical, as the detection algorithm functions
+    # at the moment.
+    assert sorted(numerical_columns) == ["column_a", "column_b", "column_d", "column_e", "column_f"]
 
     # Higher threshold
     categorical_columns, numerical_columns = infer_categorical_and_numerical_columns(
         TEST_DATAFRAME, categorical_threshold=4
     )
     assert sorted(categorical_columns) == ["column_c", "column_d", "column_e"]
-    assert sorted(numerical_columns) == ["column_a", "column_b"]
+    assert sorted(numerical_columns) == ["column_a", "column_b", "column_f"]
