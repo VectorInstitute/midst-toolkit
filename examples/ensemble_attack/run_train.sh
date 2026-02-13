ade43dc58eb75ac87c46d5ab67080720dbfbca02
skodeiri <skodeiri@gpu027.cluster.local>
2026-02-13 14:45:43 -0500

    Added files

--- examples/ensemble_attack/run_train.sh
+++ examples/ensemble_attack/run_train.sh
@@ -6,10 +6,10 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:a100:1
 #SBATCH --mem=210G
-#SBATCH --job-name=train10k
+#SBATCH --job-name=train_size
 #SBATCH --output=%j_%x.out
 #SBATCH --error=%j_%x.err
-#SBATCH --time=14:00:00
+#SBATCH --time=20:00:00
 
 
 echo "Total memory allocated: $(($SLURM_MEM_PER_NODE / 1024)) GB"
@@ -21,6 +21,6 @@ which python
 
 echo "Experiments Launched"
 
-python -m examples.ensemble_attack.run_attack  --config-name=experiment_config_10k
+python -m examples.ensemble_attack.run_attack  --config-name=experiment_config_20k_size.yaml
 
 echo "Experiments Completed"
