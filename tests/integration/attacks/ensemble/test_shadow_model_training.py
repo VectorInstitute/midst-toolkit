5fa4fefba4b933d5f6aec763345395b05abaf268
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2026-03-09 15:23:05 -0400

    Fixing test assertions

--- tests/integration/attacks/ensemble/test_shadow_model_training.py
+++ tests/integration/attacks/ensemble/test_shadow_model_training.py
@@ -15,7 +15,6 @@ from midst_toolkit.attacks.ensemble.rmia.shadow_model_training import (
     train_shadow_on_half_challenge_data,
 )
 from midst_toolkit.attacks.ensemble.shadow_model_utils import (
-    TabDDPMTrainingResult,
     fine_tune_tabddpm_and_synthesize,
     save_additional_training_config,
     train_tabddpm_and_synthesize,
@@ -68,17 +67,10 @@ def test_train_fine_tuned_shadow_models(cfg: DictConfig, tmp_path: Path) -> None
 
     assert len(shadow_data["fine_tuning_sets"]) == 2  # n_models
     assert len(shadow_data["fine_tuned_results"]) == 2  # n_models
-    for result in shadow_data["fine_tuned_results"]:
-        assert type(result) is TabDDPMTrainingResult
-        assert result.synthetic_data is not None
-        assert result.tables is not None
-        assert result.models is not None
-        assert result.configs is not None
-        assert result.save_dir is not None
-        assert result.relation_order is not None
-        assert result.all_group_lengths_probabilities is not None
-        assert type(result.synthetic_data) is pd.DataFrame
-        assert len(result.synthetic_data) == 5
+    for synthetic_data in shadow_data["fine_tuned_results"]:
+        assert type(synthetic_data) is pd.DataFrame
+        assert synthetic_data is not None
+        assert len(synthetic_data) == 5
 
     # Fine tuning sets should be disjoint
     assert set(shadow_data["fine_tuning_sets"][0]).isdisjoint(set(shadow_data["fine_tuning_sets"][1]))
@@ -114,17 +106,9 @@ def test_train_shadow_on_half_challenge_data(cfg: DictConfig, tmp_path: Path) ->
 
     assert len(shadow_data["selected_sets"]) == 2  # n_models
     assert len(shadow_data["trained_results"]) == 2  # n_models
-    for result in shadow_data["trained_results"]:
-        assert type(result) is TabDDPMTrainingResult
-        assert result.synthetic_data is not None
-        assert result.tables is not None
-        assert result.models is not None
-        assert result.configs is not None
-        assert result.save_dir is not None
-        assert result.relation_order is not None
-        assert result.all_group_lengths_probabilities is not None
-        assert type(result.synthetic_data) is pd.DataFrame
-        assert len(result.synthetic_data) == 5
+    for synthetic_data in shadow_data["trained_results"]:
+        assert type(synthetic_data) is pd.DataFrame
+        assert len(synthetic_data) == 5
 
     # Training sets should be disjoint
     assert set(shadow_data["selected_sets"][0]).isdisjoint(set(shadow_data["selected_sets"][1]))
