e50ca2704406f453f383b70467f719941386297d
ftavakoli <t.fateme@gmail.com>
2026-07-18 14:58:03 -0400

    Final Berka rerun configs added

--- examples/ensemble_attack/run_test_20k.sh
+++ examples/ensemble_attack/run_test_20k.sh
@@ -4,15 +4,13 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:a100:1
-#SBATCH --mem=180G
+#SBATCH --gres=gpu:a40:1
+#SBATCH --mem=68G
 #SBATCH --job-name=test_20k_44
 #SBATCH --output=%j_%x_%a.out
 #SBATCH --error=%j_%x_%a.err
-#SBATCH --time=19:00:00
-#SBATCH --mail-user=fatemeh.tavakoli@vectorinstitute.ai
- #SBATCH --mail-type=END,FAIL
-#SBATCH --array=0-0
+#SBATCH --time=2:00:00
+#SBATCH --array=0-18
 
 # This script sets up the environment and runs the ensemble attack test script.
 source .venv/bin/activate
@@ -23,8 +21,8 @@ which python
 # Map SLURM_ARRAY_TASK_ID to target_model_id.
 
 # # TARGET_IDS=(61 62 63 64 65 66 67 68 69 70)
-TARGET_IDS=(61)
-# TARGET_IDS=( 62 63 64 65 66 67 68 69 70 101 102 103 104 105 106 107 108 109 110)  # List of target IDs
+# TARGET_IDS=(61)
+TARGET_IDS=( 62 63 64 65 66 67 68 69 70 101 102 103 104 105 106 107 108 109 110)  # List of target IDs
 
 
 TARGET_ID=${TARGET_IDS[$SLURM_ARRAY_TASK_ID]}
